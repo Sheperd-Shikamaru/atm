7072836c6f66cb77899843334c5f9602aed6ddec
Sheperd-epf <sheperd@epfinnovationcampus.co.za>
2023-08-24 18:56:27 +0200

    Save status

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
+from transactions.models import Status
+
 from .models import BankAccountType, User, UserAddress, UserBankAccount
+
 
 
 admin.site.register(BankAccountType)
 admin.site.register(User)
 admin.site.register(UserAddress)
 admin.site.register(UserBankAccount)
+admin.site.register(Status)
